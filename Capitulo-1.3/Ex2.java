b044f7d721f8c2b5d7e9e602d0b8e788a6fda363
Victor <victorluiz.monteiro@gmail.com>
2018-02-16 00:43:44 -0200

    Finalizando Capitulo-1.3

---
+++ Capitulo-1.3/Ex2.java
@@ -0,0 +1,10 @@
+class Ex2{
+    public static void main (String[] args){
+        System.out.println(args);
+        System.out.println(args.length);
+        System.out.println(args[0]);
+
+
+
+    }
+}
---
+++ Capitulo-1.3/b/A.java
@@ -0,0 +1,6 @@
+package b;
+class A{
+    public static void main(String... args){
+        System.out.println("Rodou");
+    }
+}
